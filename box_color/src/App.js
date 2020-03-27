d6a7fa1a7ca83c809136fdbb28fa279d503410a9
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-26 22:01:15 -0500

    cleaned, changed function app to class

--- box_color/src/App.js
+++ box_color/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
+export default class App extends Component{
+
+  render(){
+
+    return(
     <div className="App">
       
-    </div>
-  );
+      
+    </div>)
+  }
+  
 }
 
-export default App;
+
