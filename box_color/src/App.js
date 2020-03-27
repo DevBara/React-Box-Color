1960dfff03baab4174fea8c39c86f693740b8eb5
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-26 22:28:19 -0500

    CSS, click counter

--- box_color/src/App.js
+++ box_color/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import './App.css';
 
-export default class App extends Component{
+
+  export default class App extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        color: 'purple',
+        clickCounter: 0
+      }
+    }
 
   render(){
 
     return(
     <div className="App">
-      
-      
+      <div className="Box" style ={{backgroundColor: this.state.color}}>
+    </div>
+      <button>Click Me!</button>
+      <p>Click Counter:</p>
+
     </div>)
   }
   
